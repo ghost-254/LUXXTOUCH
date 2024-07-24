d4b67f9ef35a1df3c9a6cc7562f753679be9a2b3
ghost-254 <nevilleotieno6@gmail.com>
2024-07-24 06:48:10 -0600

    Updated the sitemap.

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteUrl: 'https://www.luxxtouch.com', // Your website URL
+    generateRobotsTxt: true, // Generate robots.txt file
+    // optional
+    robotsTxtOptions: {
+      policies: [{ userAgent: '*', allow: '/' }],
+    },
+  };
+  
