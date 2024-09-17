f0102b5be91b419bedf2dc60a36d8c926516ca39
ghost-254 <nevilleotieno6@gmail.com>
2024-09-17 07:40:23 -0600

    update on google analytics

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,25 +1,19 @@
-// GoogleAnalytics.tsx
-
 import React from 'react';
 import Script from 'next/script';
 
 const GoogleAnalytics = () => {
   return (
     <>
-      <Script
-        strategy='lazyOnload'
-        src={`https://www.googletagmanager.com/gtag/js?id=G-F771H03F9X`}
-      />
-
-      <Script id='' strategy='lazyOnload'>
+      {/* Google tag (gtag.js) */}
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-F771H03F9X"></Script>
+      <Script id="google-analytics">
         {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-F771H03F9X', {
-              page_path: window.location.pathname,
-              });
-          `}
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-F771H03F9X');
+        `}
       </Script>
     </>
   );
